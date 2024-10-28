247f9cb10a7efd0f2331e74ffc75a98d8776e245
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2024-10-28 19:32:47 +0100

    feat: simplify deeplink updates, use cloudflare worker (#58)
    * use asdf, add wrangler dep
    
    * add typescript, test worker and worker types
    
    * remove some now legacy code
    
    * remove now unused deps
    
    * remove now unused scripts and publish config
    
    * remove now unused auto jobs
    
    * add the real worker code
    
    * delete now unused json file
    
    * update desc and remove unnecessary fields
    
    * add tsconfig
    
    * add basic wrangler config
    
    * add basic test, fix configs
    
    * first pass at release circle config
    
    * add jest junit reporter dep
    
    * add test command
    
    * use the real paths
    
    * update the test
    
    * update the readme
    
    * more consistent naming, cleanup missing newlines
    
    * remove unnecessary comments

--- server.js
+++
@@ -1,7 +0,0 @@
-var app = require('./app');
-
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-});
